e38238531aa5ad338cae973164ac8cb51edeca49
LukeDuken <Cafasso.luca@live.it>
2023-02-26 23:15:23 +0100

    Polished cloud script
    added comments

--- Kingdom/Assets/Script/Clouds.cs
+++ Kingdom/Assets/Script/Clouds.cs
@@ -10,24 +10,24 @@ public class Clouds : MonoBehaviour
     void Start()
     {
         startPos = transform.position.x;
+        //get the x lenght of the cloud sprite
         lenght = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //float temp = (transform.position.x * (1 - parallaxEffect));
+        //calculate the distance over time it moves
         float dist = (transform.position.x + (CloudSpeed * Time.deltaTime));
 
-
+        //move it 
         transform.position = new Vector2(startPos + dist, transform.position.y);
-        //transform.position = new Vector2(startPos + dist + (parallaxEffect * Time.deltaTime), transform.position.y);
-        //transform.position = new Vector2(transform.position.x + (parallaxEffect * Time.deltaTime), transform.position.y);
+        
 
-
+        // if the distance becomes major of the start position + the lenght of the sprite
         if (dist > startPos + lenght) 
         {
-            //startPos += lenght;
+            //Reset the position to the origin with a pixel pefeect transition
             transform.position = new Vector2(startPos, transform.position.y);
           
         }
